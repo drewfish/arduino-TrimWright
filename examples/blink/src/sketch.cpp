26539f62191120e49b573eac7c7362ab5f86d307
Drew Folta <drew@folta.net>
2014-01-24 06:41:34 -0800

    stripped hardware model from examples/blink

--- examples/blink/src/sketch.cpp
+++ examples/blink/src/sketch.cpp
@@ -4,64 +4,47 @@
 using namespace DFActors;
 
 
-class Hardware {
-    private:
-        static const uint32_t TIMEOUT = 500; // milliseconds
-        uint32_t timeout;
-    public:
-        void setup();
-        void loop();
-        void setLED(bool on);
-        void sleep();
-} hw;
-
-
 enum {
     SIG_TIMER = SIG_USER
 };
 
 
-class Blink : public HSM {
+class Blink : public FSM {
     public:
         DispatchOutcome stateON(const Event* event) {
             switch (event->signal) {
                 case SIG_ENTER:
-                    hw.setLED(true);
-                    return FSM_HANDLED();
-                case SIG_IDLE:
-                    hw.sleep();
+                    digitalWrite(13, HIGH);
                     return FSM_HANDLED();
                 case SIG_TIMER:
                     return FSM_TRANSITION(&Blink::stateOFF);
-                default:
-                    return HSM_SUPER(&Blink::stateROOT);
             }
         }
 
         DispatchOutcome stateOFF(const Event* event) {
             switch (event->signal) {
                 case SIG_ENTER:
-                    hw.setLED(false);
-                    return FSM_HANDLED();
-                case SIG_IDLE:
-                    hw.sleep();
+                    digitalWrite(13, LOW);
                     return FSM_HANDLED();
                 case SIG_TIMER:
                     return FSM_TRANSITION(&Blink::stateON);
-                default:
-                    return HSM_SUPER(&Blink::stateROOT);
             }
         }
 } blink;
 
 
-void Hardware::setup() {
+const uint32_t TIMEOUT = 500;   // milliseconds
+uint32_t timeout;
+
+
+void setup() {
     pinMode(13, OUTPUT);
     timeout = millis() + TIMEOUT;
+    blink.init((State) &Blink::stateON);
 }
 
 
-void Hardware::loop() {
+void loop() {
     uint32_t now = millis();
     if (now >= timeout) {
         timeout = now + TIMEOUT;
@@ -69,28 +52,6 @@ void Hardware::loop() {
         event.signal = (Signal) SIG_TIMER;
         blink.dispatch(&event);
     }
-}
-
-
-void Hardware::setLED(bool on) {
-    digitalWrite(13, (on ? HIGH : LOW));
-}
-
-
-void Hardware::sleep() {
-    // FUTURE:  put into a low power mode that still is woken by the timer
-    // used by millis()
-}
-
-
-void setup() {
-    hw.setup();
-    blink.init((State) &Blink::stateON);
-}
-
-
-void loop() {
-    hw.loop();
 }
 
 
